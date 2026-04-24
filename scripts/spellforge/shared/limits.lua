d1de2a4676c1a06cebf394bdd1962d176873e08c
slowchu <134579951+slowchu@users.noreply.github.com>
2026-04-23 17:46:57 -0700

    Add cast-authorization diagnostics for generated shell spells

---
+++ scripts/spellforge/shared/limits.lua
@@ -0,0 +1,9 @@
+local limits = {
+    MAX_RECURSION_DEPTH = 3,
+    MAX_PROJECTILES_PER_CAST = 32,
+    MAX_CHAIN_HOPS = 5,
+    MAX_SCAN_RADIUS = 2048,
+    MAX_JOBS_PER_TICK = 16,
+}
+
+return limits
